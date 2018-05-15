5485f1ca7bb2cacbf1a2adf13fb6a68c80238134
raphael de seroux <rdeseroux@gmail.com>
2018-05-15 16:02:27 +0200

    exercice 1 ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+
+  # get "/tasks", to: "tasks#index", as: :tasks
+  # get "/tasks/:id", to: "tasks#show", as: :task
+  # get "/tasks/new", to: "tasks#new", as: :new
+  # post "tasks", to: "tasks#create", as: :create
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_095216) do
+ActiveRecord::Schema.define(version: 2018_05_15_100020) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
   end
 
 end
